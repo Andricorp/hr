4d1171f4cfc5a5599b437ebe6a71ad1937386ac0
Andricorp <nacigulev@gmail.com>
2019-06-24 19:48:58 +0300

    Updates

---
+++ precache-manifest.67f17d19d337e097600ad0b9fffcd00f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc56b11a1abaedb52dbf5932c04b0e3b",
+    "url": "/hr/index.html"
+  },
+  {
+    "revision": "511f5a37ed7f7da43c44",
+    "url": "/hr/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/hr/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "511f5a37ed7f7da43c44",
+    "url": "/hr/static/js/main.2a8f6377.chunk.js"
+  },
+  {
+    "revision": "7a2153de0e897bba7db5",
+    "url": "/hr/static/js/runtime~main.a9f7fb6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hr/static/media/logo.5d5d9eef.svg"
+  }
+]);
